dbf8fcd91cfb76c7b7b85a0f6a95a79805e9708d
arisharma43 <70657612+arisharma43@users.noreply.github.com>
2023-01-28 20:07:06 -0600

    Add firebase, camera slice

---
+++ src/features/appSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const {incrementByAmount } = appSlice.actions;
+
+export const selectApp = (state) => state.counter.value;
+
+export const incrementIfOdd = (amount) => (dispatch, getState) => {
+  const currentValue = selectApp(getState());
+  if (currentValue % 2 === 1) {
+    dispatch(incrementByAmount(amount));
+  }
+};
+
+export default appSlice.reducer;
---
+++ src/features/cameraSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+  status: "idle",
+};
+
+export const cameraSlice = createSlice({
+  name: "camera",
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    setCameraImage: (state, action) => {
+      state.cameraImage += action.payload;
+    },
+    resetCameraImage: (state) => {
+      state.cameraImage = null;
+    }
+  },
+});
+
+export const { setCameraImage, resetCameraImage } = cameraSlice.actions;
+
+export const selectCameraIamge = (state) => state.camera.cameraImage;
+
+export default cameraSlice.reducer;
